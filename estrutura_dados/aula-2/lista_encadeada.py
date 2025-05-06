fe29b4a58afcc3715d40f8c45e8c5cdb403bac28
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2025-05-06 19:13:45 -0300

    fix e add testes

--- estrutura_dados/aula-2/lista_encadeada.py
+++ estrutura_dados/aula-2/lista_encadeada.py
@@ -2,7 +2,7 @@ class PlayList:
     head:'Faixa'
 
     def __init__(self, faixa:'Faixa'):
-        self.head = faixa    
+        self.head = faixa
     
     def adcionar(self, faixa:'Faixa', noInicio=True):
         if(self.head == None):
@@ -22,15 +22,22 @@ class PlayList:
         if( achou != None):
             anterior = achou.anterior
             proximo = achou.proximo
-            anterior.proximo = proximo
+
+            if anterior is not None:
+                anterior.proximo = proximo
+            else:
+                self.head = proximo
+
+            if proximo is not None:
+                proximo.anterior = anterior
+
 
     def buscar(self, id:int):
-        if(self.head != None):
-            item = self.head
-            while item.id != id:
-                item = item.proximo
+        item = self.head
+        while item != None:
             if(item.id == id):
                 return item
+            item = item.proximo
 
 
 class Faixa:
@@ -40,19 +47,29 @@ class Faixa:
     def __init__(self, id:int, titulo:str):
         self.id = id
         self.titulo = titulo
+        self.proximo = None
+        self.anterior = None
     
 if __name__ == "__main__":
     faixa1 = Faixa(1, "Victory Leap")
     faixa2 = Faixa(2, "Killing in the Name")
     faixa3 = Faixa(3, "Hidropônica")
     faixa4 = Faixa(4, "Que pais é esse")
+    faixa5 = Faixa(5, "The Decline")
 
     playlist = PlayList(faixa1)
     playlist.adcionar(faixa2)
     playlist.adcionar(faixa3, False)
     playlist.adcionar(faixa4, False)
+    playlist.adcionar(faixa5)
 
-    print(playlist.head.titulo)  # Victory Leap
-    print(playlist.head.proximo.titulo)  # Killing in the Name
+    print(playlist.buscar(5).titulo)  # The Decline
+    playlist.remover(5)
+    print(playlist.buscar(5))  # None
+    print('---------')
+
+
+    print(playlist.head.titulo)  # Killing in the Name
+    print(playlist.head.proximo.titulo)  # Victory Leap
     print(playlist.head.proximo.proximo.titulo)  # Hidropônica
     print(playlist.head.proximo.proximo.proximo.titulo)  # Que pais é esse
